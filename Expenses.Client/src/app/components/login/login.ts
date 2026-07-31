fb1ae843a078caa03fe8bbf900d8d28a5f1c12e8
Blackbaud-VishnuVardhanReddyManikonda <VishnuVardhanReddy.Manikonda@blackbaud.com>
2026-07-31 12:14:51 +0530

    Add standalone property to login component

--- Expenses.Client/src/app/components/login/login.ts
+++ Expenses.Client/src/app/components/login/login.ts
@@ -6,6 +6,7 @@ import { AuthService } from '../../services/auth';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
   imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
