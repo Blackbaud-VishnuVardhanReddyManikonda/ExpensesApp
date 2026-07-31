407baf6acb847e91f2b7634063cf9cba275e30b5
Blackbaud-VishnuVardhanReddyManikonda <VishnuVardhanReddy.Manikonda@blackbaud.com>
2026-07-31 12:39:25 +0530

    Refactor App component and update template/styles

--- Expenses.Client/src/app/app.ts
+++ Expenses.Client/src/app/app.ts
@@ -1,18 +1,15 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Login } from './components/login/login';
-import { TransactionForm } from './components/transaction-form/transaction-form';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
-import { TransactionList } from './components/transaction-list/transaction-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header,Footer],
   standalone: true,
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [RouterOutlet, Header, Footer],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class App {
-  protected readonly title = signal('Expenses.Client');
+  title = 'Expense Tracker';
 }
