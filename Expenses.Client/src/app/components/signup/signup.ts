7a0a36b65a17571d8e3c9dd58fb43b927a9c40f2
Blackbaud-VishnuVardhanReddyManikonda <VishnuVardhanReddy.Manikonda@blackbaud.com>
2026-07-31 12:17:34 +0530

    Fix formatting in signup component metadata

--- Expenses.Client/src/app/components/signup/signup.ts
+++ Expenses.Client/src/app/components/signup/signup.ts
@@ -6,7 +6,7 @@ import { AuthService } from '../../services/auth';
 
 @Component({
   selector: 'app-signup',
-  standalone: true
+  standalone: true,
   imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './signup.html',
   styleUrl: './signup.css',
