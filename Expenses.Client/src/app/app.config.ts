93afda28e6bd298820ef51208462a8b0194010cb
Blackbaud-VishnuVardhanReddyManikonda <VishnuVardhanReddy.Manikonda@blackbaud.com>
2026-07-31 13:20:08 +0530

    Simplify HTTP client configuration in app config

--- Expenses.Client/src/app/app.config.ts
+++ Expenses.Client/src/app/app.config.ts
@@ -18,23 +18,13 @@
 // };
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import {
-  provideHttpClient,
-  withInterceptors,
-  withXsrfConfiguration
-} from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(
-      withInterceptors([authInterceptor]),
-      withXsrfConfiguration({
-        cookieName: 'XSRF-TOKEN',
-        headerName: 'X-XSRF-TOKEN',
-      })
-    )
+    provideHttpClient(withInterceptors([authInterceptor]))
   ]
 };
